3761a41a41491fe38c185eced41cc1645205722b
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-07-23 09:11:40 -0400

    ng new PhotoBucket --routing

---
+++ PhotoBucket/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { PhotoBucketPage } from './app.po';
+
+describe('photo-bucket App', () => {
+  let page: PhotoBucketPage;
+
+  beforeEach(() => {
+    page = new PhotoBucketPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
